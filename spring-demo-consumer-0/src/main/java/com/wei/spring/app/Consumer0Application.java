7db274693ad556efdc7f316a723402637b0434f1
Wang Wei <wangwei2@myhexin.com>
2018-12-27 20:54:57 +0800

    modify feign package name

--- spring-demo-consumer-0/src/main/java/com/wei/spring/app/Consumer0Application.java
+++ spring-demo-consumer-0/src/main/java/com/wei/spring/app/Consumer0Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
--- spring-demo-consumer/src/main/java/com/wei/spring/app/service/ProductService.java
+++ spring-demo-consumer/src/main/java/com/wei/spring/app/service/ProductService.java
@@ -2,7 +2,8 @@ package com.wei.spring.app.service;
 
 import com.wei.spring.app.api.ProductApi;
 import com.wei.spring.app.model.dto.ResultDTO;
-import org.springframework.cloud.netflix.feign.FeignClient;
+
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
