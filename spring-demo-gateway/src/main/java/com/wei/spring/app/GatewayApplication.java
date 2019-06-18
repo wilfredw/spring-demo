e117079c53b4f7e2e3eb40ff0a4ff1a7fb9e8f29
Wang Wei <wangwei2@myhexin.com>
2019-06-18 16:07:02 +0800

    gateway

---
+++ spring-demo-gateway/src/main/java/com/wei/spring/app/GatewayApplication.java
@@ -0,0 +1,12 @@
+package com.wei.spring.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by viruser on 2019/6/18.
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+    }
+}
