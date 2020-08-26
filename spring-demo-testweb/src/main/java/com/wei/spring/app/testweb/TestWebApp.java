db3488ca49f42618117ab40e3347343021077189
Wang Wei <wilfred.w.w@gmail.com>
2020-08-27 07:50:32 +0800

    add test web

---
+++ spring-demo-testweb/src/main/java/com/wei/spring/app/testweb/TestWebApp.java
@@ -0,0 +1,11 @@
+package com.wei.spring.app.testweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWebApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TestWebApp.class, args);
+    }
+}
