0964d165d66cc32960e3ac019f14033645fb5cb5
wangwei <well_nature@163.com>
2021-04-02 12:05:50 +0800

    a

--- spring-demo-aop/src/main/java/com/wei/spring/aop/controller/UserController.java
+++ spring-demo-aop/src/main/java/com/wei/spring/aop/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.wei.spring.aop.controller;
 
-import com.netflix.discovery.converters.Auto;
 import com.wei.spring.aop.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
